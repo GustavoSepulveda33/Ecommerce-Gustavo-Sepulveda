86d288c9c0bfb313498de6dff8213e4ebda25ce0
Gustavo Sepulveda <112096626+GustavoSepulveda33@users.noreply.github.com>
2022-10-10 13:33:46 -0400

    Contact Form Validation, Fixed some <a> tag links

--- ContactPage/Contact.js
+++ ContactPage/Contact.js
@@ -10,15 +10,45 @@ function warpHome() {
 };
 
 
+
+
+
 // FORM VALIDATION
 
+function validateform(){  
 
-function getVal() {
-    const val = document.querySelector('#name1').value;
-    console.log(val);
-}
+    //var pull input value for Full Name box  
+    var name = document.querySelector(`[name="Fullname"]`) ; 
+    var validator = name.value ; 
 
 
+    // Number.isInteger() takes the value in the () and returns "true" if it is a number and "false" if it is NaN (i.e. a string)
+    // Although prompt turns everything into a string, parseInt will only convert into integers up until the first character thats not a number. So parseInt(123abc) = 123 and parseInt(gustavo) = NaN; 
+    
+    let filter = parseInt(validator);
+    let filterCheck = Number.isInteger(filter); 
+
+
+    // console.log(filterCheck)
+
+
+        if (filterCheck == true) {
+
+            console.log("numeric/ not a string");
+            alert(`Invalid input entry. "Full Name" entry only accepts words. Please try again`) ;
+            return false
+
+            // <form onsumbit=`return validateform()` states that if this function returns false, the form submission will be cancelled. 
+            //      In this case, it will only return false if the Full Name input includes anything that is not a string. 
+
+        } else {
+
+            return true ; 
+
+        }
+
+
+    }
 
 
 
